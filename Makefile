b55cfd1b267885f1fc9811940a49e6b7291b23fa
Pete Wall <pete.wall@grafana.com>
2025-09-03 14:05:02 -0500

    Basic working theory
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

---
+++ Makefile
@@ -0,0 +1,30 @@
+
+kubeconfig.yaml:
+	kind create cluster --name fm-test-cluster
+
+.PHONY: create-cluster delete-cluster
+create-cluster: kubeconfig.yaml
+delete-cluster:
+	kind delete cluster --name fm-test-cluster
+	rm -f kubeconfig.yaml
+
+deploy-alloy-operator: kubeconfig.yaml
+	helm upgrade --install alloy-operator grafana/alloy-operator --namespace monitoring --create-namespace
+
+pipelines/fleet-management.yaml:
+	echo "---" > $@
+	echo "host: $(shell op read "op://Lab/Fleet Management Manager Token/website")" >> $@
+	echo "username: $(shell op read "op://Lab/Fleet Management Manager Token/username")" >> $@
+	echo "password: $(shell op read "op://Lab/Fleet Management Manager Token/password")" >> $@
+
+deployments/remote-config-credentials.yaml:
+	echo "---" > $@
+	kubectl create secret generic remote-config-credentials \
+		--namespace monitoring \
+		--from-literal=url="$(shell op read "op://Lab/Fleet Management Cluster Token/website")" \
+		--from-literal=username="$(shell op read "op://Lab/Fleet Management Cluster Token/username")" \
+		--from-literal=password="$(shell op read "op://Lab/Fleet Management Cluster Token/password")" \
+		-o yaml --dry-run=client >> $@
+
+deploy: kubeconfig.yaml deploy-alloy-operator deployments/remote-config-credentials.yaml
+	kubectl apply -f deployments
---
+++ pipelines/sync-pipelines.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+fleetManagementFile="${SCRIPT_DIR}/fleet-management.yaml"
+if [ ! -f "${fleetManagementFile}" ]; then
+  echo "${fleetManagementFile} not found!"
+  echo "Try running make pipelines/fleet-management.yaml"
+  exit 1
+fi
+
+host=$(yq eval '.host' "${fleetManagementFile}")
+username=$(yq eval '.username' "${fleetManagementFile}")
+password=$(yq eval '.password' "${fleetManagementFile}")
+
+pipelinesFile="${SCRIPT_DIR}/pipelines.yaml"
+if [ ! -f "${pipelinesFile}" ]; then
+  echo "${pipelinesFile} not found!"
+  exit 1
+fi
+
+numPipelines=$(yq eval '.pipelines | length' "${pipelinesFile}")
+for i in $(seq 0 $((numPipelines - 1))); do
+  pipelineName=$(yq eval ".pipelines[$i].name" "${pipelinesFile}")
+  echo "Syncing pipeline: ${pipelineName}"
+
+  pipelineMatchers=$(yq eval --output-format json --indent 0 ".pipelines[$i].matchers" "${pipelinesFile}")
+  pipelineFilename=$(yq eval ".pipelines[$i].file" "${pipelinesFile}")
+  pipelineFile="${SCRIPT_DIR}/${pipelineFilename}"
+  if [ ! -f "${pipelineFile}" ]; then
+    echo "Pipeline file ${pipelineFile} not found!"
+    exit 1
+  fi
+
+  upsertPipelineRequestBody=$(
+    jq --null-input \
+      --arg name "${pipelineName}" \
+      --arg contents "$(cat "${pipelineFile}")" \
+      --argjson matchers "${pipelineMatchers}" \
+      --argjson enabled true \
+     '.pipeline = {name: $name, contents: $contents, matchers: $matchers, enabled: $enabled}')
+
+  curl -X POST \
+    --header "Content-Type: application/json" \
+    --user "${username}:${password}" \
+    --data "${upsertPipelineRequestBody}" \
+    "${host}/pipeline.v1.PipelineService/UpsertPipeline" \
+    --show-error --fail
+
+    sleep 1
+done
